301500c971d4e8eec3d07bb06da2d396cda03e37
Yevgeny Getman <yevgeny.getman@icims.com>
2020-10-29 23:27:32 -0400

    refactor and addTrain class

--- perceptron.py
+++ perceptron.py
@@ -117,82 +117,123 @@ class Network(Neuron):
 	def getOutput(self):
 		return self.output
 
-	def calculateCost(self,expectedOutput): 
+
+class Run(Network):
+
+	def __init__(self,net,dataset,verbose = False):
+			
+		self.net = net
+		self.dataset = dataset
+		self.verbose = verbose
+
+
+	def calculateCost(self,output,expectedOutput): 
 
 		# compute the cost of a training example
-		assert len(self.output) == len(expectedOutput)
+		assert len(output) == len(expectedOutput)
 
 		cost = 0
 		for i in range(len(output)): 
-			cost += (self.output[i] - expectedOutput[i])**2
+			cost += (output[i] - expectedOutput[i])**2
 		return cost
 
+	def computeTotalCost(self):
+		
+		images = self.dataset['images']
+		labels = self.dataset['labels']
+
+		totalCost = 0
+		for i,image in enumerate(images):
+			result = self.run(image,labels[i])
+			totalCost += result['cost']
+		totalCost = totalCost / len(images)
+
+		return totalCost
+
+
+	def run(self,image,label):
+
+		# run a single image example
+		
+		image = self.normalizeMNIST(image)
+
+		if(self.verbose): print 'image label:',label
+
+		# run the network
+		self.net.runNetwork(image)
+
+		# get the output
+		output = self.net.getOutput()
+		if(self.verbose): print 'output:', self.decodeOutput(output)
+
+		# compute the cost
+		expectedOutput = self.encodeOutput(label)
+		# print 'expected output:', expectedOutput
+		cost = self.calculateCost(output,expectedOutput)
+		if(self.verbose): print 'cost:', cost
+
+		return { 'output' : output, 'cost' : cost }
+
+
+	def encodeOutput(self,number):
+
+		output = []
+
+		for i in range(10): 
+			if i == number:
+				output.append(1)
+			else: output.append(0)
+
+		return output
+		
+	def decodeOutput(self,outputLayerActivations):
+
+		greatest_num = 0
+		for i in range(len(outputLayerActivations)): 
+			if outputLayerActivations[i] > greatest_num: 
+				greatest_num = outputLayerActivations[i]
+				guess = i
+		return guess
+
+	def normalizeMNIST(self,image):
+
+		for i in range(len(image)):
+			if(image[i] > 0):
+				image[i] = (255 - float(image[i]))/255
+		return image
+		
 
 def sigmoid(x):
 	return 1 / (1 + math.exp(-x))
 
-def normalizeMNIST(image):
 
-	for i in range(len(image)):
-		if(image[i] > 0):
-			image[i] = (255 - float(image[i]))/255
-	return image
 
-def encodeOutput(number):
-
-	output = []
-
-	for i in range(10): 
-		if i == number:
-			output.append(1)
-		else: output.append(0)
-
-	return output
-	
-def decodeOutput(outputLayerActivations):
-
-	greatest_num = 0
-	for i in range(len(outputLayerActivations)): 
-		if outputLayerActivations[i] > greatest_num: 
-			greatest_num = outputLayerActivations[i]
-			guess = i
-	return guess
 
 
 # ------ run below here -------
 
+# import dataset
 mndata = MNIST('./mnist')
 images, labels = mndata.load_training()
 
-# choose a random image
-test_image_index = random.randint(1, len(images)-1)
-testImg = normalizeMNIST(images[test_image_index])
-testImgValue = labels[test_image_index]
-print 'image:',testImgValue
-
-input = testImg # [] to initialize with random values
-
-inputLayerSize = len(testImg)
+chunk_size = 10
 
 # initialize the network
-net = Network([inputLayerSize,16,16,10],sigmoid)
+net = Network([784,16,16,10],sigmoid)
 net.initializeNetwork()
 
 # run the network
-net.runNetwork(input)
+run = Run(net, {	
+	'images' : images[:chunk_size],
+	'labels' : labels[:chunk_size]
+},True)
 
-# get the output
-output = net.getOutput()
-print 'output:', decodeOutput(output)
-
-# compute the cost
-expectedOutput = encodeOutput(testImgValue)
-# print 'expected output:', testImgValue
-cost = net.calculateCost(expectedOutput)
-print 'cost:', cost
+cost = run.computeTotalCost()
+print 'Total cost:', cost
 
 
-
+# test = run.run(images[0],labels[0])
+# print test
 
 
 
